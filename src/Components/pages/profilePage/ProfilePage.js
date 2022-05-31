51563bb6ac6fc9d39fce247fdf40e86c26b70332
ZimmermannnnamremmiZ <Maximvo92@gmail.com>
2022-05-31 10:53:41 +0300

    profilePage styles

--- src/Components/pages/profilePage/ProfilePage.js
+++ src/Components/pages/profilePage/ProfilePage.js
@@ -12,20 +12,22 @@ import './profilePage.scss';
 const ProfilePage = () => {
   const id = useId();
   const isMounted = useRef();
-  const PostsRefs = useRef([]);
+  const postsRefs = useRef([]);
   const {userId} = useParams();
   const [data, setData] = useState({user: false, posts: []});
+  const [active, setActive] = useState(false)
+
   const {getUser, getUserPosts} = useApi();
 
   // при клике на элемент с ref, добавит класс selected
   const onPostClick = (id) => {
-    PostsRefs.current.forEach(item => item.classList.remove('post__item_selected'))
-    PostsRefs.current[id].classList.add('post__item_selected')
-    PostsRefs.current[id].focus()
+    postsRefs.current.forEach(item => item.classList.remove('post__item_selected'))
+    postsRefs.current[id].classList.add('post__item_selected')
+    postsRefs.current[id].focus()
   }
 
   const onPostOver = () => {
-    PostsRefs.current.forEach(item => item.classList.remove('post__item_selected'))
+    postsRefs.current.forEach(item => item.classList.remove('post__item_selected'))
   }
 
   const fetchData = async () => {
@@ -63,9 +65,9 @@ const ProfilePage = () => {
             </div>
             <div className='profile__data-space'></div>
           </div>
-              <ul className='profile__data-posts'>
+              <ul className={active ? 'profile__data-posts--active' : 'profile__data-posts'} onClick={() => setActive(!active)}>
                 {/* как в задании, список из 3-х превью */}
-                {data.posts.map((el, i) => i<3 ? <li key={el.id}>{el.title}</li> : null)}
+                {data.posts.map(el => <li key={el.id}>{el.title}</li>)}
               </ul>
           <div className='post'>
             <h2 className='post__title'>Посты</h2>
@@ -78,20 +80,22 @@ const ProfilePage = () => {
                             {group.map((post) => (
                               <div className='post__item'
                                    key={post.id}
-                                   ref= {el => PostsRefs.current[post.id] = el}
+                                   ref= {el => postsRefs.current[post.id] = el}
                                    onClick={() => {onPostClick(post.id)}}
                                    onMouseOver={onPostOver}
                               >
                                 <h6 className='post__item-title'>{post.title}</h6>
-                                <p id='post-item' className='post__item-body'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima corporis, unde consequatur dolores quibusdam magnam a! Repellendus facilis debitis alias aliquam, impedit modi velit maxime eveniet cum aperiam ullam repudiandae. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio maiores vero iure officiis expedita cumque neque minima soluta in, quis quam, nobis odio culpa praesentium debitis temporibus itaque quasi illum!
-                                Obcaecati laborum aperiam repudiandae! Cum dicta quam dolores laborum quia suscipit omnis maiores magnam quas incidunt totam, deleniti consequatur quasi provident repellendus harum voluptate! Voluptas vel illum reprehenderit exercitationem dolorem.
-                                Fugit animi quam ad, labore eius modi repellat veniam fuga reprehenderit illo quas eligendi ipsum eos officia sunt praesentium neque quia. Assumenda voluptas ea voluptatem numquam consectetur! Iure, blanditiis praesentium?
-                                Consequuntur reprehenderit totam consequatur dignissimos fugiat. Reprehenderit corrupti totam illum, pariatur sed quasi doloremque commodi facere, nulla vero voluptas quod minus a perferendis error ipsa, id odio alias. Odio, adipisci.
-                                Id rem asperiores sint vitae error magnam, pariatur nobis quibusdam soluta minima? Unde adipisci laborum ipsum quod, aut asperiores tempora quos deserunt quis dolore, odio voluptatibus ut, ratione tenetur possimus.
-                                Labore nulla dolorem nam quod cum repellat facilis. A eos, consequatur, rem ut neque quae velit totam porro similique corporis quos fugit illo rerum delectus! Perspiciatis pariatur quisquam assumenda itaque.
-                                Velit non obcaecati eligendi ipsum eveniet laborum quod itaque voluptatum delectus ad temporibus a fuga inventore in, neque placeat doloribus error nobis ea cupiditate quis. Deserunt distinctio itaque repellendus dolorem?
-                                Ducimus, optio! Corrupti voluptate obcaecati fuga debitis deleniti esse sed sequi! Quidem veritatis fuga ea culpa voluptatem in. Ipsum aut vel dolores laborum sequi amet ratione perferendis obcaecati quisquam. Aspernatur?
-                                Sed doloremque consectetur eveniet repellat unde error, reiciendis possimus iste! Numquam voluptas rem, est exercitationem mollitia ex nesciunt quasi. Nisi aspernatur doloribus eum quibusdam, porro minus minima deserunt impedit natus.</p>
+                                <div className='post__item-date'>12.01.22</div>
+                                <p id='post-item' className='post__item-body'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dolores nemo commodi soluta omnis repellendus neque perferendis pariatur eum? Veritatis hic, quod reprehenderit soluta quas facere unde numquam error minus?
+                                Ducimus placeat incidunt suscipit voluptates voluptatum blanditiis provident repellat numquam, quia dignissimos harum corrupti ipsa ab? Fugit laborum excepturi aliquam esse dolorem similique quod magnam reiciendis, eos incidunt architecto ad!
+                                Doloremque, veniam provident laudantium repudiandae odit quidem nihil quas id, velit vitae aspernatur quisquam. Blanditiis ullam, harum sapiente quo dolor qui? Tempore quaerat totam fugiat dolorum maxime omnis sapiente consequuntur!
+                                Culpa molestiae minus iure odio nulla rerum qui ipsa ipsum reprehenderit molestias illum, numquam voluptatum possimus ad sint distinctio assumenda, in tempore praesentium impedit quo nemo accusamus. Dolore, accusamus quidem?
+                                Illum ipsa possimus animi autem nam. Magnam laboriosam necessitatibus ducimus accusamus possimus culpa temporibus fugiat harum corporis suscipit porro voluptate quibusdam optio quos recusandae, placeat nobis repellendus ratione non animi.
+                                Perspiciatis, culpa. A mollitia beatae optio vero ea dolores possimus amet officia quaerat earum maiores, repudiandae, dignissimos asperiores molestias nihil ad animi totam ducimus ab veniam accusamus! Optio, quibusdam nulla.
+                                Animi modi obcaecati debitis iure minus iste corrupti ad error. Minus in atque ratione repellat sunt fuga velit, laboriosam, iusto dolores voluptatibus nihil voluptatem minima dignissimos magnam dolorum pariatur est!
+                                Non in error quia iusto, quam quas impedit ullam nulla et veritatis similique earum libero ipsum voluptates deserunt cumque vero ex nisi ut natus! Consectetur minima quia ut sit deleniti?
+                                Doloremque doloribus quos, corporis tenetur quae odio voluptatum aliquam voluptatibus perspiciatis, minima incidunt architecto nisi fugit ducimus harum quis, laborum ab officia mollitia repudiandae minus. Quisquam fugiat eligendi vero unde.
+                                Eveniet obcaecati nostrum officiis consequatur vel voluptates commodi saepe natus, sequi explicabo doloribus odio asperiores ipsum et molestias inventore, quam deleniti distinctio cum aperiam quos labore dolores! Tempore, perspiciatis eos!</p>
                                 {/* {post.body} */}
                               </div>
                             ))}
